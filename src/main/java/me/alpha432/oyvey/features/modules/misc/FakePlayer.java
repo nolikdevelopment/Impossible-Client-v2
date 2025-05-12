2e0e586547243411fb9904be61af720aaeaf7a32
nolikdevelopment <knik7536@yandex.ru>
2025-05-12 15:51:04 +0300

    Add files via upload

---
+++ src/main/java/me/alpha432/oyvey/features/modules/misc/FakePlayer.java
@@ -0,0 +1,28 @@
+package me.alpha432.oyvey.features.modules.misc;
+
+import com.mojang.authlib.GameProfile;
+import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.features.settings.Setting;
+import net.minecraft.client.network.OtherClientPlayerEntity;
+import net.minecraft.entity.Entity;
+
+import java.util.UUID;
+
+public class FakePlayer extends Module {
+    private Setting<String> name = str("Name","ImpossibleClient");
+    private OtherClientPlayerEntity player;
+    public FakePlayer() {
+        super("FakePlayer", "", Category.MISC, true,false,false);
+    }
+    @Override public void onEnable() {
+        if (fullNullCheck()) return;
+        player = new OtherClientPlayerEntity(mc.world, new GameProfile(UUID.randomUUID(), name.getValue()));
+        player.copyFrom(mc.player);
+        mc.world.addEntity(player);
+        mc.player.setHealth(20);
+    }
+    @Override public void onDisable() {
+        mc.world.removeEntity(player.getId(), Entity.RemovalReason.DISCARDED);
+    }
+
+}
