852253dfdfcc913ebcea4b51fee66729f7fe53c6
nolikdevelopment <knik7536@yandex.ru>
2025-05-14 21:56:51 +0300

    ура я сделал этот ебучий рендер модуль с 3 попытки

---
+++ src/main/java/me/alpha432/oyvey/features/modules/render/FullBright.java
@@ -0,0 +1,32 @@
+package me.alpha432.oyvey.features.modules.render;
+
+import me.alpha432.oyvey.features.modules.Module;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+
+public class FullBright extends Module {
+
+    public FullBright() {
+        super("FullBright", "", Category.RENDER, true, false, false);
+    }
+
+    @Override
+    public void onEnable() {
+        if (nullCheck()) return;
+        if (!mc.player.hasStatusEffect(StatusEffects.NIGHT_VISION)) {
+            mc.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, StatusEffectInstance.INFINITE));
+
+
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        if (nullCheck()) return;
+        if (mc.player.hasStatusEffect(StatusEffects.NIGHT_VISION)) {
+            mc.player.removeStatusEffect(StatusEffects.NIGHT_VISION);
+        }
+
+    }
+}
+
