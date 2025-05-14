be20e02ab2c95e7561c311b617dcdb5697bac138
nolikdevelopment <knik7536@yandex.ru>
2025-05-14 21:58:02 +0300

    callbackInfo.cancel(); такая мразь

---
+++ src/main/java/me/alpha432/oyvey/mixin/LightmapTextureManagerMixin.java
@@ -0,0 +1,22 @@
+package me.alpha432.oyvey.mixin;
+
+import me.alpha432.oyvey.OyVey;
+import me.alpha432.oyvey.features.modules.render.FullBright;
+import net.minecraft.client.render.LightmapTextureManager;
+import org.spongepowered.asm.mixin.Mixin;
+
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LightmapTextureManager.class)
+
+public class LightmapTextureManagerMixin {
+
+    @Inject(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/SimpleFramebuffer;endWrite()V", shift = At.Shift.BEFORE))
+    private void update$endWrite(float delta, CallbackInfo callbackInfo) {
+        FullBright fullBright = OyVey.moduleManager.getModuleByClass(FullBright.class);
+        if (fullBright.isEnabled());
+
+    }
+}
