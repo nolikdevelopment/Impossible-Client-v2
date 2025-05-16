3b7a7eac459324aa1461ba8dfd726c011027f5c0
nolikdevelopment <knik7536@yandex.ru>
2025-05-16 23:22:29 +0300

    вроде работает, но в идее ошибка какая то, лол

--- src/main/java/me/alpha432/oyvey/features/modules/render/FullBright.java
+++ src/main/java/me/alpha432/oyvey/features/modules/render/FullBright.java
@@ -1,5 +1,7 @@
 package me.alpha432.oyvey.features.modules.render;
 
+import com.google.common.eventbus.Subscribe;
+import me.alpha432.oyvey.event.impl.PacketEvent;
 import me.alpha432.oyvey.features.modules.Module;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
@@ -26,6 +28,12 @@ public class FullBright extends Module {
         if (mc.player.hasStatusEffect(StatusEffects.NIGHT_VISION)) {
             mc.player.removeStatusEffect(StatusEffects.NIGHT_VISION);
         }
+
+    }
+    @Subscribe
+    public void onPacket(PacketEvent.Send event) {
+        if (!mc.player.hasStatusEffect(StatusEffects.NIGHT_VISION));
+        mc.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, StatusEffectInstance.INFINITE));
 
     }
 }
