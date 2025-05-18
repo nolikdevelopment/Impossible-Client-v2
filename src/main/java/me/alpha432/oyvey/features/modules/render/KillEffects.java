4b21add455bb4a4847eeab49ed15f281aef0467b
nolikdevelopment <knik7536@yandex.ru>
2025-05-18 09:13:10 +0300

    йоу

---
+++ src/main/java/me/alpha432/oyvey/features/modules/render/KillEffects.java
@@ -0,0 +1,19 @@
+package me.alpha432.oyvey.features.modules.render;
+
+import com.google.common.eventbus.Subscribe;
+import me.alpha432.oyvey.features.modules.Module;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LightningEntity;
+import net.minecraft.entity.player.PlayerEntity;
+
+public class KillEffects extends Module {
+    public KillEffects() {
+        super("KillEffects", "", Category.RENDER, true,false, false);
+    }
+    @Subscribe public void onDeath(PlayerEntity entity) {
+        LightningEntity lightningEntity = new LightningEntity(EntityType.LIGHTNING_BOLT, mc.world);
+        lightningEntity.refreshPositionAfterTeleport(entity.getX(), entity.getY(), entity.getZ());
+        mc.world.addEntity(lightningEntity);
+
+    }
+}
