300167877f03b4d0dd4c8ea05bb9f828ae51da91
nolikdevelopment <knik7536@yandex.ru>
2025-05-20 21:09:20 +0300

    ура нахуй

---
+++ src/main/java/me/alpha432/oyvey/features/modules/misc/NoRotate.java
@@ -0,0 +1,27 @@
+package me.alpha432.oyvey.features.modules.misc;
+
+import com.google.common.eventbus.Subscribe;
+import me.alpha432.oyvey.event.impl.PacketEvent;
+import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.mixin.PlayerPositionMixin;
+import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
+import net.minecraft.network.packet.s2c.play.PositionFlag;
+
+public class NoRotate extends Module {
+    public NoRotate() {
+        super("NoRotate", "", Category.MISC, true, false, false);
+    }
+
+    @Subscribe
+    public void onPacket(PacketEvent.Receive event) {
+        if (nullCheck()) return;
+        if (event.getPacket() instanceof PlayerPositionLookS2CPacket packet) {
+            ((PlayerPositionMixin) (Object) packet.change()).setYaw(mc.player.getYaw());
+            ((PlayerPositionMixin) (Object) packet.change()).setPitch(mc.player.getPitch());
+            packet.relatives().remove(PositionFlag.X_ROT);
+            packet.relatives().remove(PositionFlag.Y_ROT);
+
+
+        }
+    }
+}
