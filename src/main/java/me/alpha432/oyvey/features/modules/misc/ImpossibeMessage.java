ee6ff6279475b15e4c86e69bec7b245be1b22453
nolikdevelopment <knik7536@yandex.ru>
2025-05-24 12:25:40 +0300

    улучшения +fix NoRotate

--- src/main/java/me/alpha432/oyvey/features/modules/misc/ImpossibeMessage.java
+++ src/main/java/me/alpha432/oyvey/features/modules/misc/ImpossibeMessage.java
@@ -1,15 +1,24 @@
 package me.alpha432.oyvey.features.modules.misc;
 
 import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.features.settings.Setting;
+import me.alpha432.oyvey.util.models.Timer;
 
 public class ImpossibeMessage extends Module {
+    private Setting<Integer> delay = num("Delay:", 5, 1, 30);
+    private Timer timer = new Timer();
     private String ChatSuffix = "";
 
     public ImpossibeMessage() {
-        super("ImpossibleMessage", "", Category.MISC, true,false,false);
+        super("ImpossibleMessage", "", Category.MISC, true, false, false);
     }
-    @Override public void onEnable() {
-        mc.player.networkHandler.sendChatMessage("I just killed Vetmo Team using my client!" + ChatSuffix);
-        disable();
+
+    @Override
+    public void onTick() {
+        if (fullNullCheck()) return;
+        if (timer.passedS(delay.getValue())) {
+            mc.player.networkHandler.sendChatMessage("I just killed Vetmo Team using my client!" + ChatSuffix);
+            timer.reset();
+        }
     }
 }
