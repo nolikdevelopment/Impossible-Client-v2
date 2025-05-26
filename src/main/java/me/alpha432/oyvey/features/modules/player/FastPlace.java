5be43bacc4f2cf5868c9fe856df79b6fd00e5902
nolikdevelopment <knik7536@yandex.ru>
2025-05-26 22:24:27 +0300

    бля это 2 модуль где я забыл закрыть скобку))

--- src/main/java/me/alpha432/oyvey/features/modules/player/FastPlace.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/FastPlace.java
@@ -2,7 +2,10 @@ package me.alpha432.oyvey.features.modules.player;
 
 import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.settings.Setting;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPos;
 
 public class FastPlace extends Module {
     public Setting<Boolean> exp = bool("Exp", true);
@@ -12,20 +15,16 @@ public class FastPlace extends Module {
     public FastPlace() {
         super("FastPlace", "Makes you throw exp faster", Category.PLAYER, true, false, false);
     }
-
-    @Override
-    public void onUpdate() {
+    @Override public void onUpdate() {
         if (nullCheck()) return;
-
+        if (mc.player.isHolding(Items.END_CRYSTAL) && crystals.getValue()) {
+            mc.itemUseCooldown = 0;
+        }
+        if (mc.player.isHolding(Items.OBSIDIAN) && obsidian.getValue()) {
+            mc.itemUseCooldown = 0;
+        }
         if (mc.player.isHolding(Items.EXPERIENCE_BOTTLE) && exp.getValue()) {
             mc.itemUseCooldown = 0;
-            if (mc.player.isHolding(Items.END_CRYSTAL) && crystals.getValue()) {
-                mc.itemUseCooldown = 0;
-                if (mc.player.isHolding(Items.OBSIDIAN) && obsidian.getValue()) {
-                    mc.itemUseCooldown = 0;
-
-                }
-            }
         }
     }
 }
