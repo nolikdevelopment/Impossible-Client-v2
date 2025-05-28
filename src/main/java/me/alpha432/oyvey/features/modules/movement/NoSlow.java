ec24b93f893919673258e48b6da342fc175d91b3
nolikdevelopment <knik7536@yandex.ru>
2025-05-28 22:54:46 +0300

    updated

--- src/main/java/me/alpha432/oyvey/features/modules/movement/NoSlow.java
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/NoSlow.java
@@ -2,18 +2,29 @@ package me.alpha432.oyvey.features.modules.movement;
 
 import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.settings.Setting;
-import net.minecraft.component.DataComponentTypes;
+import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+
 
 public class NoSlow extends Module {
     public Setting<Boolean> food = bool("Food", true);
+    public Setting<Boolean> portals = bool("Portals", true);
+    public Setting<Boolean> guimove = bool("GuiMove", true);
+
     public NoSlow() {
-        super("NoSlow", "", Category.MOVEMENT, true,false,false);
+        super("NoSlow", "", Category.MOVEMENT, true, false, false);
     }
-    @Override
-    public void onUpdate() {
-        mc.options.sneakKey.setPressed(false);
-    }
-    public void noSlow() {
-        if (!food.getValue()&& mc.player.getActiveItem().getComponents().contains(DataComponentTypes.FOOD));
+    @Override public void onTick() {
+        if (fullNullCheck()) return;
+        if (guimove.getValue() && mc.currentScreen != null && !(mc.currentScreen instanceof ChatScreen)) {
+            for (KeyBinding binding : new KeyBinding[]{mc.options.forwardKey, mc.options.sneakKey, mc.options.jumpKey, mc.options.backKey, mc.options.sprintKey, mc.options.leftKey, mc.options.rightKey}) {
+                binding.setPressed(InputUtil.isKeyPressed(mc.getWindow().getHandle(), InputUtil.fromTranslationKey(binding.getBoundKeyTranslationKey()).getCode()));
+
+            }
+        }
     }
 }
+
+
+
