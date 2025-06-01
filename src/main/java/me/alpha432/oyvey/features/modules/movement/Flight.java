bea17b37ddcfa1d2a9e86c424648c9492e61864b
nolikdevelopment <knik7536@yandex.ru>
2025-06-01 17:04:35 +0300

    улучшения, пока что хз как вертикалспид сделать

--- src/main/java/me/alpha432/oyvey/features/modules/movement/Flight.java
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/Flight.java
@@ -1,26 +1,33 @@
 package me.alpha432.oyvey.features.modules.movement;
 
-import com.google.common.eventbus.Subscribe;
 import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.settings.Setting;
 
 
 public class Flight extends Module {
-    private final Setting<Float> speed = num("Speed:", 1f, 0.5f, 3f);
+    public Setting<Flight.Mod> mode = this.register(new Setting<>("Mode", Mod.Vanilla));
+    private final Setting<Integer> speed = num("Speed:", 4, 1, 6);
+    public enum Mod {
+        Vanilla, Static
+    }
     public Flight() {
-        super("Flight", "", Category.MOVEMENT, true,false,false);
+        super("Flight", "", Category.MOVEMENT, true, false, false);
 
     }
-    @Override
-    public void onUpdate() {
-        mc.player.getAbilities().setFlySpeed((float) speed.getValue()/ 11);
-        mc.player.getAbilities().flying = true;
+    @Override public void onTick() {
+        switch (mode.getValue()) {
+            case Vanilla -> {
+                mc.player.getAbilities().flying = true;
+                mc.player.getAbilities().setFlySpeed((float) (speed.getValue()) / 11);
+            }
+            case Static -> {
+                mc.player.getAbilities().setFlySpeed((float) (speed.getValue()));
+                mc.player.getAbilities().flying = true;
+                if (mc.options.jumpKey.isPressed()) mc.player.getAbilities().setFlySpeed((float) (speed.getValue() / 4));
+                if (mc.options.sneakKey.isPressed()) mc.player.getAbilities().setFlySpeed((float) (speed.getValue() / 4));
+                mc.player.setVelocity(0, 0, 0);
+            }
+        }
 
-
-
-    }
-    @Override
-    public void onDisable() {
-        mc.player.getAbilities().flying = false;
     }
 }
