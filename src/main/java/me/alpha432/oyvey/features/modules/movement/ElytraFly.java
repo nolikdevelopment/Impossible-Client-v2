82ca986c1ad24b156dc9f83223a9490bcc14a699
nolikdevelopment <knik7536@yandex.ru>
2025-06-01 21:44:29 +0300

    ура новый модуль фенкс фабрик апи енд феинхак

---
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/ElytraFly.java
@@ -0,0 +1,47 @@
+package me.alpha432.oyvey.features.modules.movement;
+
+import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.features.settings.Setting;
+import net.minecraft.util.math.Vec3d;
+
+
+public class ElytraFly extends Module {
+    private final Setting<Float> vertical = num("Vertical", 4f, 1f, 6f);
+    private final Setting<Float> horizontal = num("Horizontal", 4f, 1f, 6f);
+    public ElytraFly() {
+        super("ElytraFly", "", Category.MOVEMENT, true,false,false);
+    }
+    @Override public void onTick() {
+        if (nullCheck()) return;
+        if (mc.player.isGliding()) {
+            Vec3d moveVec = Vec3d.ZERO;
+            Float y = 0f;
+
+            if (mc.options.forwardKey.isPressed()) {
+                moveVec = moveVec.add(Vec3d.fromPolar(0, mc.player.getYaw()).normalize());
+            }
+            if (mc.options.backKey.isPressed()) {
+                moveVec = moveVec.add(Vec3d.fromPolar(0, mc.player.getYaw() + 180).normalize());
+            }
+
+            if (mc.options.rightKey.isPressed()) {
+                moveVec = moveVec.add(Vec3d.fromPolar(0, mc.player.getYaw() + 90).normalize());
+            }
+            if (mc.options.leftKey.isPressed()) {
+                moveVec = moveVec.add(Vec3d.fromPolar(0, mc.player.getYaw() - 90).normalize());
+            }
+
+            if (mc.options.jumpKey.isPressed()) {
+                y = vertical.getValue();
+            }
+
+            if (mc.options.sneakKey.isPressed()) {
+                y = -vertical.getValue();
+            }
+
+
+            moveVec = moveVec.normalize().multiply(horizontal.getValue());
+            mc.player.setVelocity(moveVec.x,y, moveVec.z);
+        }
+    }
+}
