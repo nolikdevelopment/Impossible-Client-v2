3f382dcdf6e7eb422649b92382e72389ffe13fc6
nolikdevelopment <knik7536@yandex.ru>
2025-06-02 20:26:24 +0300

    новый модуль

--- src/main/java/me/alpha432/oyvey/mixin/CameraMixin.java
+++ src/main/java/me/alpha432/oyvey/mixin/CameraMixin.java
@@ -2,12 +2,18 @@ package me.alpha432.oyvey.mixin;
 
 import me.alpha432.oyvey.OyVey;
 import me.alpha432.oyvey.features.modules.render.NoRender;
+import me.alpha432.oyvey.features.modules.render.ViewClip;
 import net.minecraft.block.enums.CameraSubmersionType;
 import net.minecraft.client.render.Camera;
+import net.minecraft.entity.Entity;
+import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(Camera.class)
 public class CameraMixin {
@@ -17,5 +23,17 @@ public class CameraMixin {
         ci.setReturnValue(CameraSubmersionType.NONE);
         if (noRender.isEnabled()&& noRender.liquidOverlay.getValue())
             ci.cancel();
+    }
+    @ModifyArgs(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/Camera;clipToSpace(F)F"))
+    private void update(Args args) {
+        ViewClip viewClip = OyVey.moduleManager.getModuleByClass(ViewClip.class);
+        if (viewClip.isEnabled()&& viewClip.player.getValue())
+            args.set(0, OyVey.moduleManager.getModuleByClass(ViewClip.class).range.getValue().floatValue());
+    }
+    @Inject(method = "clipToSpace", at = @At("HEAD"), cancellable = true)
+    private void aVoid(float f, CallbackInfoReturnable<Float> cir) {
+        ViewClip viewClip = OyVey.moduleManager.getModuleByClass(ViewClip.class);
+        if (viewClip.isEnabled())
+            cir.setReturnValue(f);
     }
 }
