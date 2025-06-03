7d7cdabf6ec8a6049b07b47de15b97b18910ffe8
nolikdevelopment <knik7536@yandex.ru>
2025-06-03 15:52:45 +0300

    добавил проверку на наличии в друзьях атакуемого

--- src/main/java/me/alpha432/oyvey/features/modules/combat/Aura.java
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/Aura.java
@@ -1,13 +1,18 @@
 package me.alpha432.oyvey.features.modules.combat;
 
+import me.alpha432.oyvey.OyVey;
 import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.settings.Setting;
+import me.alpha432.oyvey.util.MathUtil;
 import me.alpha432.oyvey.util.models.Timer;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.boss.WitherEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.SwordItem;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.Vec3d;
 
 public class Aura extends Module {
     private Timer timer = new Timer();
@@ -41,11 +46,16 @@ public class Aura extends Module {
                     MobEntity mob = (MobEntity) entity;
                     attackEntity(mob);
                 }
+                if (entity instanceof WitherEntity && mobs.getValue()) {
+                    MobEntity mob = (MobEntity) entity;
+                    attackEntity(mob);
+                }
             }
     }
 
     private void attackEntity(Entity entity) {
         if (timer.passedS(0.8)) {
+            if (OyVey.friendManager.isFriend(entity.getName().getString())) return;
             mc.interactionManager.attackEntity(mc.player, entity);
             mc.player.swingHand(Hand.MAIN_HAND);
             timer.reset();
