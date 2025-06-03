850d249326f5f6efa3c38a5e08669e55d904747f
nolikdevelopment <knik7536@yandex.ru>
2025-06-03 15:42:07 +0300

    хз вроде норм

--- src/main/java/me/alpha432/oyvey/mixin/MinecraftClientMixin.java
+++ src/main/java/me/alpha432/oyvey/mixin/MinecraftClientMixin.java
@@ -4,28 +4,40 @@ import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import me.alpha432.oyvey.OyVey;
 import me.alpha432.oyvey.features.modules.exploit.MultiTask;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.client.network.ClientPlayerEntity;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
-public class MinecraftClientMixin {
-    @Shadow
-    @Nullable
-    public ClientPlayerInteractionManager interactionManager;
+public abstract class MinecraftClientMixin {
+
+    @Shadow @Nullable public ClientPlayerEntity player;
+
+    @Shadow public abstract boolean forcesUnicodeFont();
 
     @ModifyExpressionValue(method = "doItemUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerInteractionManager;isBreakingBlock()Z"))
     private boolean doItemUseModifyIsBreakingBlock(boolean original) {
-        return !OyVey.moduleManager.getModuleByClass(MultiTask.class).isEnabled() && original;
+        MultiTask multiTask = OyVey.moduleManager.getModuleByClass(MultiTask.class);
+        if(multiTask.isEnabled()) return false;
+        return original;
+
     }
 
     @ModifyExpressionValue(method = "handleBlockBreaking", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isUsingItem()Z"))
     private boolean handleBlockBreakingModifyIsUsingItem(boolean original) {
-        return !OyVey.moduleManager.getModuleByClass(MultiTask.class).isEnabled() && original;
+        MultiTask multiTask = OyVey.moduleManager.getModuleByClass(MultiTask.class);
+        if(multiTask.isEnabled()) return false;
+        return original;
     }
+    @ModifyExpressionValue(method = "handleInputEvents", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isUsingItem()Z", ordinal = 0))
+    private boolean handleInputEventsModifyIsUsingItem(boolean original) {
+        MultiTask multiTask = OyVey.moduleManager.getModuleByClass(MultiTask.class);
+        if(multiTask.isEnabled()) return false;
+        return original;
+
+    }
+
 
         }
