bf09591dd4888b67d0153b935476558dd3a4457e
nolikdevelopment <knik7536@yandex.ru>
2025-06-05 17:54:28 +0300

    новый модуль

--- src/main/java/me/alpha432/oyvey/mixin/MixinEntity.java
+++ src/main/java/me/alpha432/oyvey/mixin/MixinEntity.java
@@ -1,12 +1,16 @@
 package me.alpha432.oyvey.mixin;
 
 import me.alpha432.oyvey.OyVey;
+import me.alpha432.oyvey.features.modules.exploit.AntiVanish;
 import me.alpha432.oyvey.features.modules.player.Velocity;
+import me.alpha432.oyvey.features.modules.render.NoRender;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 
 @Mixin(Entity.class)
@@ -14,11 +18,40 @@ public class MixinEntity {
     @Inject(method = "pushAwayFrom", at = @At("HEAD"), cancellable = true)
     public void pushAwayFrom(Entity entity, CallbackInfo callbackInfo) {
         Velocity velocity = OyVey.moduleManager.getModuleByClass(Velocity.class);
-        if (velocity.isEnabled()&& velocity.entityPush.getValue()) {
+        if (velocity.isEnabled() && velocity.entityPush.getValue()) {
             callbackInfo.cancel();
 
         }
 
     }
 
-}
+    @Inject(method = "isOnFire", at = @At("HEAD"), cancellable = true)
+    public void fire(CallbackInfoReturnable<Boolean> cir) {
+        NoRender noRender = OyVey.moduleManager.getModuleByClass(NoRender.class);
+        if (noRender.isEnabled() && noRender.fire.getValue()) {
+            cir.setReturnValue(false);
+        }
+
+
+    }
+
+    @Inject(method = "isInvisible", at = @At("HEAD"), cancellable = true)
+    private void rendervanish(CallbackInfoReturnable<Boolean> cir) {
+        AntiVanish antiVanish = OyVey.moduleManager.getModuleByClass(AntiVanish.class);
+        if (antiVanish != null && antiVanish.isEnabled()) {
+            cir.setReturnValue(false);
+        }
+    }
+
+
+        @Inject(method = "isInvisibleTo", at = @At("HEAD"), cancellable = true)
+        private void vanish (PlayerEntity player, CallbackInfoReturnable < Boolean > cir){
+            AntiVanish antiVanish = OyVey.moduleManager.getModuleByClass(AntiVanish.class);
+            if (antiVanish != null && antiVanish.isEnabled()) {
+                cir.setReturnValue(false);
+            }
+
+        }
+    }
+
+
