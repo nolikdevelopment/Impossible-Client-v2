850d18601676fa01da487d25acf8d7683d80a78e
nolikdevelopment <knik7536@yandex.ru>
2025-06-09 00:25:37 +0300

    урааа

--- src/main/java/me/alpha432/oyvey/features/modules/client/Notifications.java
+++ src/main/java/me/alpha432/oyvey/features/modules/client/Notifications.java
@@ -1,10 +1,45 @@
 package me.alpha432.oyvey.features.modules.client;
 
+import me.alpha432.oyvey.OyVey;
+import me.alpha432.oyvey.features.commands.Command;
 import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.features.settings.Setting;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Formatting;
+
+import java.util.ArrayList;
 
 public class Notifications extends Module {
+    private final ArrayList<PlayerEntity> rangeplayers = new ArrayList<>();
+    public Setting<Boolean> visualrange = bool("VisualRange", true);
+
     public Notifications() {
-        super("Notifications", "", Category.CLIENT, true,false,false);
+        super("Notifications", "", Category.CLIENT, true, false, false);
     }
 
+    @Override
+    public void onUpdate() {
+        if (fullNullCheck()) return;
+        if (visualrange.getValue()) {
+            for (Entity entity : mc.world.getEntities()) {
+                if (!(entity instanceof PlayerEntity player) || entity == mc.player) continue;
+                if (!rangeplayers.contains(player)) {
+                    rangeplayers.add(player);
+                    String message = "" + Formatting.WHITE + player.getName().getString() + Formatting.RED + " has entered your visual range!";
+                    Command.sendMessage(message);
+                }
+            }
+            if (!rangeplayers.isEmpty()) {
+                for (PlayerEntity player : new ArrayList<>(rangeplayers)) {
+                    if (!mc.world.getPlayers().contains(player)) {
+                        rangeplayers.remove(player);
+                        String message = "" + Formatting.WHITE + player.getName().getString() + Formatting.RED + " has left your visual range!";
+                        Command.sendMessage(message);
+                    }
+                }
+            }
+
+        }
+    }
 }
