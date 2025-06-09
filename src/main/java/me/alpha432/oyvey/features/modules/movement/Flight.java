b81f6def6d1485395e2ea4004f4ad366ae596798
nolikdevelopment <knik7536@yandex.ru>
2025-06-09 13:03:04 +0300

    вертикалка и горизонталка, баг с лево и право

--- src/main/java/me/alpha432/oyvey/features/modules/movement/Flight.java
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/Flight.java
@@ -2,9 +2,12 @@ package me.alpha432.oyvey.features.modules.movement;
 
 import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.settings.Setting;
+import net.minecraft.util.math.Vec3d;
 
 
 public class Flight extends Module {
+    private final Setting<Float> horizontal = num("Vertical", 4f, 1f, 6f);
+    private final Setting<Float> vertical = num("Horizontal", 4f, 1f, 6f);
     public Setting<Flight.Mod> mode = this.register(new Setting<>("Mode", Mod.Vanilla));
     private final Setting<Integer> speed = num("Speed:", 4, 1, 6);
     public enum Mod {
@@ -17,8 +20,30 @@ public class Flight extends Module {
     @Override public void onTick() {
         switch (mode.getValue()) {
             case Vanilla -> {
-                mc.player.getAbilities().flying = true;
-                mc.player.getAbilities().setFlySpeed((float) (speed.getValue()) / 11);
+                Vec3d move = Vec3d.ZERO;
+                Float y = 0f;
+                if (mc.options.forwardKey.isPressed()) {
+                    move = move.add(Vec3d.fromPolar(0, mc.player.getYaw()).normalize());
+                }
+                if (mc.options.backKey.isPressed()) {
+                    move = move.add(Vec3d.fromPolar(0, mc.player.getYaw()).normalize());
+                }
+                if (mc.options.rightKey.isPressed()) {
+                    move = move.add(Vec3d.fromPolar(0, mc.player.getYaw()).normalize());
+                }
+                if (mc.options.leftKey.isPressed()) {
+                    move = move.add(Vec3d.fromPolar(0, mc.player.getYaw()).normalize());
+                }
+                if (mc.options.jumpKey.isPressed()) {
+                    y = vertical.getValue();
+
+                }
+                if (mc.options.sneakKey.isPressed()) {
+                    y = -vertical.getValue();
+
+                }
+                move = move.normalize().multiply(horizontal.getValue());
+                mc.player.setVelocity(move.x, y, move.z);
             }
             case Static -> {
                 mc.player.getAbilities().setFlySpeed((float) (speed.getValue()));
