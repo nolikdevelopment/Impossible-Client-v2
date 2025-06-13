0e3f7a92f4bd2435ff0afbfb72f2e15f88955a65
nolikdevelopment <knik7536@yandex.ru>
2025-06-13 21:20:36 +0300

    фикс скоростей, 1 режим полета, крч хоть что то

--- src/main/java/me/alpha432/oyvey/features/modules/movement/Flight.java
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/Flight.java
@@ -9,50 +9,47 @@ public class Flight extends Module {
     private final Setting<Float> horizontal = num("Vertical", 4f, 1f, 6f);
     private final Setting<Float> vertical = num("Horizontal", 4f, 1f, 6f);
     public Setting<Flight.Mod> mode = this.register(new Setting<>("Mode", Mod.Vanilla));
-    private final Setting<Integer> speed = num("Speed:", 4, 1, 6);
+
     public enum Mod {
-        Vanilla, Static
+        Vanilla,
     }
+
     public Flight() {
         super("Flight", "", Category.MOVEMENT, true, false, false);
 
     }
-    @Override public void onTick() {
+
+    @Override
+    public void onTick() {
         switch (mode.getValue()) {
-            case Vanilla -> {
-                Vec3d move = Vec3d.ZERO;
-                Float y = 0f;
-                if (mc.options.forwardKey.isPressed()) {
-                    move = move.add(Vec3d.fromPolar(0, mc.player.getYaw()).normalize());
-                }
-                if (mc.options.backKey.isPressed()) {
-                    move = move.add(Vec3d.fromPolar(0, mc.player.getYaw()).normalize());
-                }
-                if (mc.options.rightKey.isPressed()) {
-                    move = move.add(Vec3d.fromPolar(0, mc.player.getYaw()).normalize());
-                }
-                if (mc.options.leftKey.isPressed()) {
-                    move = move.add(Vec3d.fromPolar(0, mc.player.getYaw()).normalize());
-                }
-                if (mc.options.jumpKey.isPressed()) {
-                    y = vertical.getValue();
+                case Vanilla -> {
+                    if (mc.options.forwardKey.isPressed()) {
+                       mc.player.getAbilities().setFlySpeed((float) horizontal.getValue() / 14);
+                       mc.player.getAbilities().flying = true;
+                    }
+                    if (mc.options.backKey.isPressed()) {
+                        mc.player.getAbilities().setFlySpeed((float) horizontal.getValue() / 14);
+                        mc.player.getAbilities().flying = true;
+                    }
+                    if (mc.options.rightKey.isPressed()) {
+                        mc.player.getAbilities().setFlySpeed((float) horizontal.getValue() / 14);
+                        mc.player.getAbilities().flying = true;
 
-                }
-                if (mc.options.sneakKey.isPressed()) {
-                    y = -vertical.getValue();
+                    }
+                    if (mc.options.leftKey.isPressed()) {
+                        mc.player.getAbilities().setFlySpeed((float) horizontal.getValue() / 14);
+                        mc.player.getAbilities().flying = true;
+                    }
+                    if (mc.options.jumpKey.isPressed()) {
+                        mc.player.getAbilities().setFlySpeed((float) vertical.getValue() / 14);
+                        mc.player.getAbilities().flying = true;
 
+                    }
+                    if (mc.options.sneakKey.isPressed()) {
+                        mc.player.getAbilities().setFlySpeed((float) vertical.getValue() / 1);
+                        mc.player.getAbilities().flying = true;
+                    }
                 }
-                move = move.normalize().multiply(horizontal.getValue());
-                mc.player.setVelocity(move.x, y, move.z);
-            }
-            case Static -> {
-                mc.player.getAbilities().setFlySpeed((float) (speed.getValue()));
-                mc.player.getAbilities().flying = true;
-                if (mc.options.jumpKey.isPressed()) mc.player.getAbilities().setFlySpeed((float) (speed.getValue() / 4));
-                if (mc.options.sneakKey.isPressed()) mc.player.getAbilities().setFlySpeed((float) (speed.getValue() / 4));
-                mc.player.setVelocity(0, 0, 0);
             }
         }
-
     }
-}
