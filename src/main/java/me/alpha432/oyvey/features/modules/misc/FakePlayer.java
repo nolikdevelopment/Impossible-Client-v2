c725679a9f0329033fa4bb2de6c90949430b5c99
nolikdevelopment <knik7536@yandex.ru>
2025-06-13 23:27:44 +0300

    фикс краша модуля

--- src/main/java/me/alpha432/oyvey/features/modules/misc/FakePlayer.java
+++ src/main/java/me/alpha432/oyvey/features/modules/misc/FakePlayer.java
@@ -9,20 +9,30 @@ import net.minecraft.entity.Entity;
 import java.util.UUID;
 
 public class FakePlayer extends Module {
-    private Setting<String> name = str("Name","ImpossibleClient");
+    private Setting<String> name = str("Name", "FakePlayer");
     private OtherClientPlayerEntity player;
+
     public FakePlayer() {
-        super("FakePlayer", "", Category.MISC, true,false,false);
-    }
-    @Override public void onEnable() {
-        if (fullNullCheck()) return;
-        player = new OtherClientPlayerEntity(mc.world, new GameProfile(UUID.randomUUID(), name.getValue()));
-        player.copyFrom(mc.player);
-        mc.world.addEntity(player);
-        mc.player.setHealth(20);
-    }
-    @Override public void onDisable() {
-        mc.world.removeEntity(player.getId(), Entity.RemovalReason.DISCARDED);
+        super("FakePlayer", "", Category.MISC, true, false, false);
     }
 
+    @Override
+    public void onEnable() {
+        if (fullNullCheck()) return;
+        if (mc.world != null && mc.player != null) {
+            player = new OtherClientPlayerEntity(mc.world, new GameProfile(UUID.randomUUID(), name.getValue()));
+            player.copyFrom(mc.player);
+            mc.world.addEntity(player);
+            mc.player.setHealth(20);
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        if (mc.world != null && player != null) {
+            mc.world.removeEntity(player.getId(), Entity.RemovalReason.KILLED);
+            player = null;
+        }
+    }
 }
+
