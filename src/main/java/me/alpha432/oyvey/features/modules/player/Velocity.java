52d33c972304d8c54a3a968773524534388d8d86
nolikdevelopment <knik7536@yandex.ru>
2025-06-13 17:43:45 +0300

    фикс отталкиваний в велосити
    я думал что сделал опять не работающий калл, но я затестил и в мио так же без этой настройки толкает, поэтому это уже тут пиздец бля

--- src/main/java/me/alpha432/oyvey/features/modules/player/Velocity.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/Velocity.java
@@ -7,10 +7,11 @@ import me.alpha432.oyvey.features.settings.Setting;
 import me.alpha432.oyvey.mixin.EntityVelocityUpdateS2CPacketAccessor;
 import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
 import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket;
+import net.minecraft.util.math.Vec3d;
 
 public class Velocity extends Module {
-    private final Setting<Integer> vert = num("Vertical", 100, 0, 100);
-    private final Setting<Integer> hort = num("Horizontal", 100, 0, 100);
+    private final Setting<Integer> hort = num("Vertical", 0, 0, 100);
+    private final Setting<Integer> vert = num("Horizontal", 0, 0, 100);
     public Setting<Boolean> blockPush = bool("BlockPush", true);
     public Setting<Boolean> entityPush = bool("EntityPush", true);
     public Setting<Boolean> explosion = bool("Explosion", true);
@@ -18,17 +19,30 @@ public class Velocity extends Module {
     public Velocity() {
         super("Velocity", "Removes velocity from explosions and entities", Category.PLAYER, true, false, false);
     }
-    @Subscribe public void onPacket(PacketEvent.Receive event) {
-        if (event.getPacket() instanceof EntityVelocityUpdateS2CPacket packet && packet.getEntityId() == mc.player.getId()) {
-         double x = (packet.getVelocityX() / 8000d - mc.player.getVelocity().x * hort.getValue());
-         double y = (packet.getVelocityY() / 8000d - mc.player.getVelocity().y * vert.getValue());
-         double z = (packet.getVelocityZ() / 8000d - mc.player.getVelocity().z * hort.getValue());
-            ((EntityVelocityUpdateS2CPacketAccessor) packet).setX((int) (x * 8000 + mc.player.getVelocity().x * 8000));
-            ((EntityVelocityUpdateS2CPacketAccessor) packet).setY((int) (y * 8000 + mc.player.getVelocity().y * 8000));
-            ((EntityVelocityUpdateS2CPacketAccessor) packet).setZ((int) (z * 8000 + mc.player.getVelocity().z * 8000));
 
+    @Subscribe
+    public void onPacket(PacketEvent.Receive event) {
+        if (event.getPacket() instanceof EntityVelocityUpdateS2CPacket packet && packet.getEntityId() == mc.player.getId()) {
+            Vec3d velocity = mc.player.getVelocity();
+            double x = (packet.getVelocityX() / 8000d) - (velocity.x * hort.getValue());
+            double y = (packet.getVelocityY() / 8000d) - (velocity.y * vert.getValue());
+            double z = (packet.getVelocityZ() / 8000d) - (velocity.z * hort.getValue());
+
+            ((EntityVelocityUpdateS2CPacketAccessor) packet).setX((int) ((x + velocity.x) * 8000));
+            ((EntityVelocityUpdateS2CPacketAccessor) packet).setY((int) ((y + velocity.y) * 8000));
+            ((EntityVelocityUpdateS2CPacketAccessor) packet).setZ((int) ((z + velocity.z) * 8000));
         }
+        if (explosion.getValue() && event.getPacket() instanceof ExplosionS2CPacket packet) event.cancel();
+    }
+
+    @Override
+    public String getDisplayInfo() {
+        return "H: " + hort.getValue() + " V: " + vert.getValue();
     }
 }
+
+
+
+
 
 
