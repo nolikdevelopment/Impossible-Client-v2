9916c36ac59538792502f5e3650622bd5526d501
nolikdevelopment <knik7536@yandex.ru>
2025-06-13 14:56:55 +0300

    добавлен новый модуль

---
+++ src/main/java/me/alpha432/oyvey/features/modules/misc/AutoLogin.java
@@ -0,0 +1,16 @@
+package me.alpha432.oyvey.features.modules.misc;
+
+import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.features.settings.Setting;
+
+public class AutoLogin extends Module {
+    public Setting<String> password = str("Password", "testclientlol");
+    public AutoLogin() {
+        super("AutoLogin", "", Category.MISC, true, false, false);
+    }
+    @Override public void onEnable() {
+        if (fullNullCheck()) return;
+        mc.player.networkHandler.sendChatCommand("login " + password.getValue() + " " + password.getValue());
+        disable();
+    }
+}
