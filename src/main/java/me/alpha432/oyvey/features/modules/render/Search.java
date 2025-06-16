b4486761be74833654e5b389e4338d183b072f76
nolikdevelopment <knik7536@yandex.ru>
2025-06-16 17:59:40 +0300

    обводка вокруг блока

--- src/main/java/me/alpha432/oyvey/features/modules/render/Search.java
+++ src/main/java/me/alpha432/oyvey/features/modules/render/Search.java
@@ -5,8 +5,6 @@ import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.settings.Setting;
 import me.alpha432.oyvey.util.BlockUtils;
 import me.alpha432.oyvey.util.RenderUtil;
-import net.minecraft.block.AnvilBlock;
-import net.minecraft.block.CraftingTableBlock;
 import net.minecraft.block.entity.*;
 import net.minecraft.util.math.Box;
 
@@ -20,6 +18,7 @@ public class Search extends Module {
     public Setting<Boolean> shulkers = bool("Shulkers", true);
     public Setting<Boolean> furnace = bool("Furnaces", true);
     public Setting<Boolean> barrels = bool("Barrels", true);
+    public Setting<Float> lines = this.register(new Setting<>("Line:", 1.5f, 0.1f, 5f));
     public Search() {
         super("Search", "", Category.RENDER, true,false,false);
     }
@@ -28,35 +27,33 @@ public class Search extends Module {
         if (chests.getValue()) {
             ArrayList<BlockEntity> blockEntities = BlockUtils.getTileEntities()
                     .collect(Collectors.toCollection(ArrayList::new));
-            for (BlockEntity blockEntity: blockEntities) {
+            for (BlockEntity blockEntity : blockEntities) {
                 if (blockEntity instanceof ChestBlockEntity || blockEntity instanceof TrappedChestBlockEntity || blockEntity instanceof BarrelBlockEntity) {
                     Box box = new Box(blockEntity.getPos());
-                    RenderUtil.drawBox(event.getMatrix(), box, Color.ORANGE, 0.7);
+                    RenderUtil.drawBox(event.getMatrix(), box, Color.ORANGE, lines.getValue());
+                    RenderUtil.drawBoxFilled(event.getMatrix(), box, new Color(255, 165, 0, 86));
                 }
                 if (blockEntity instanceof EnderChestBlockEntity && echests.getValue()) {
                     Box box = new Box(blockEntity.getPos());
-                    RenderUtil.drawBox(event.getMatrix(), box, Color.ORANGE, 0.7);
+                    RenderUtil.drawBox(event.getMatrix(), box, Color.ORANGE, lines.getValue());
+                    RenderUtil.drawBoxFilled(event.getMatrix(), box, new Color(255, 165, 0, 86));
                 }
                 if (blockEntity instanceof ShulkerBoxBlockEntity && shulkers.getValue()) {
                     Box box = new Box(blockEntity.getPos());
-                    RenderUtil.drawBox(event.getMatrix(), box, Color.MAGENTA, 0.7);
-
-                    }
+                    RenderUtil.drawBox(event.getMatrix(), box, Color.MAGENTA, lines.getValue());
+                    RenderUtil.drawBoxFilled(event.getMatrix(), box, new Color(255, 0, 255, 86));
+                }
                 if (blockEntity instanceof FurnaceBlockEntity && furnace.getValue()) {
                     Box box = new Box(blockEntity.getPos());
-                    RenderUtil.drawBox(event.getMatrix(), box, Color.MAGENTA, 0.7);
+                    RenderUtil.drawBox(event.getMatrix(), box, Color.MAGENTA, lines.getValue());
+                    RenderUtil.drawBoxFilled(event.getMatrix(), box, new Color(255, 0, 255, 86));
                 }
                 if (blockEntity instanceof BarrelBlockEntity && barrels.getValue()) {
                     Box box = new Box(blockEntity.getPos());
-                    RenderUtil.drawBox(event.getMatrix(), box, Color.ORANGE, 0.7);
-                }
-
-
+                    RenderUtil.drawBox(event.getMatrix(), box, Color.ORANGE, lines.getValue());
+                    RenderUtil.drawBoxFilled(event.getMatrix(), box, new Color(255, 165, 0, 86));
                 }
             }
-
         }
-
-
-
+        }
     }
