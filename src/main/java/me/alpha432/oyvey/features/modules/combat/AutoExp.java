b5c8c6e522c606df12a08d834e0317d4553e5943
nolikdevelopment <knik7536@yandex.ru>
2025-06-19 12:56:11 +0300

    улучшения

--- src/main/java/me/alpha432/oyvey/features/modules/combat/AutoExp.java
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/AutoExp.java
@@ -1,11 +1,18 @@
 package me.alpha432.oyvey.features.modules.combat;
 
+import me.alpha432.oyvey.OyVey;
 import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.features.settings.Setting;
 import me.alpha432.oyvey.util.InventoryUtil;
 import net.minecraft.item.ExperienceBottleItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.Vec3d;
 
 public class AutoExp extends Module {
+    private final Setting<Integer> prochnost = num("Prochnosh:", 20, 1, 100);
+
     public AutoExp() {
         super("AutoExp", "", Category.COMBAT, true, false, false);
     }
@@ -13,12 +20,17 @@ public class AutoExp extends Module {
     @Override
     public void onTick() {
         if (nullCheck()) return;
-        int oldslot = mc.player.getInventory().selectedSlot;
-        int slot = InventoryUtil.findHotbarItem(ExperienceBottleItem.class);
-        if (slot != -1) {
-            InventoryUtil.switchSlot(slot);
-            mc.interactionManager.interactItem(mc.player, Hand.MAIN_HAND);
-            InventoryUtil.switchSlot(oldslot);
+        for (ItemStack item : mc.player.getArmorItems()) {
+            if (InventoryUtil.armourProchnost(item) < prochnost.getValue()) {
+                int oldslot = mc.player.getInventory().selectedSlot;
+                int slot = InventoryUtil.findHotbarItem(ExperienceBottleItem.class);
+                if (slot != -1) {
+                    InventoryUtil.switchSlot(slot);
+                    mc.player.swingHand(Hand.MAIN_HAND, true);
+                    mc.interactionManager.interactItem(mc.player, Hand.MAIN_HAND);
+                    InventoryUtil.switchSlot(oldslot);
+                }
+            }
         }
     }
 }
