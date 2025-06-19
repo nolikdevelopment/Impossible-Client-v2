5c41080d4d5c7d886c89fc723b5482149fd402c2
nolikdevelopment <knik7536@yandex.ru>
2025-06-19 12:56:45 +0300

    фига че сделал

--- src/main/java/me/alpha432/oyvey/util/InventoryUtil.java
+++ src/main/java/me/alpha432/oyvey/util/InventoryUtil.java
@@ -1,16 +1,17 @@
 package me.alpha432.oyvey.util;
-
 import me.alpha432.oyvey.util.traits.Util;
+import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket;
 
 
 public class InventoryUtil implements Util {
-    public static void switchSlot(int slot){
+    public static void switchSlot(int slot) {
         mc.player.getInventory().selectedSlot = slot;
-        mc.getNetworkHandler().sendPacket(new UpdateSelectedSlotC2SPacket(slot));
+        mc.player.networkHandler.sendPacket(new UpdateSelectedSlotC2SPacket(slot));
     }
+
 
     public static int findHotbarItem(Class clazz) {
         for (int i = 0; i < 9; ++i) {
@@ -24,5 +25,17 @@ public class InventoryUtil implements Util {
             return i;
         }
         return -1;
+    }
+
+    public static int findBlockHotbar(Block block) {
+        for (int i = 0; i < 9; i++) {
+            if (mc.player.getInventory().getStack(i).getItem() == block.asItem()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static double armourProchnost(ItemStack stack) {
+        return ((double) (stack.getMaxDamage() - stack.getDamage()) /  stack.getMaxDamage()) * 100;
     }
 }
