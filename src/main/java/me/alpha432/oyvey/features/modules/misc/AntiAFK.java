7c2b3493019d29b805e47a0ce9ee7b68376aeb59
nolikdevelopment <knik7536@yandex.ru>
2025-07-11 22:48:20 +0300

    фикс снеака

--- src/main/java/me/alpha432/oyvey/features/modules/misc/AntiAFK.java
+++ src/main/java/me/alpha432/oyvey/features/modules/misc/AntiAFK.java
@@ -7,7 +7,10 @@ import me.alpha432.oyvey.util.models.Timer;
 public class AntiAFK extends Module {
     public Setting<Integer> delay = this.register(new Setting<>("Delay", 30, 1, 60));
     public Setting<Boolean> jump = bool("Jump", true);
+    public Setting<Boolean> sneak = bool("Sneak", true);
+    public Setting<Boolean> message = bool("Message", true);
     private Timer timer = new Timer();
+
 
     public AntiAFK() {
         super("AntiAFK", "", Category.MISC, true, false, false);
@@ -16,18 +19,22 @@ public class AntiAFK extends Module {
     @Override
     public void onTick() {
         if (fullNullCheck()) return;
+
         if (timer.passedS(delay.getValue())) {
             if (jump.getValue()) {
                 mc.player.jump();
-
-
             }
-
+            if (message.getValue()) {
+                mc.player.networkHandler.sendChatMessage("I dont AFK!");
+            }
+            if (sneak.getValue()) {
+                mc.options.sneakKey.setPressed(true);
+            } else {
+                mc.options.sneakKey.setPressed(false);
             }
             timer.reset();
-
+        } else {
+            mc.options.sneakKey.setPressed(false);
         }
-
     }
-
-
+}
