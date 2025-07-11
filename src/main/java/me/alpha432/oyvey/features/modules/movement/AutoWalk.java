cbc1083b9febb55f0439cf9a2766f76dbd6dc88d
nolikdevelopment <knik7536@yandex.ru>
2025-07-11 22:50:09 +0300

    фикс с нословом

--- src/main/java/me/alpha432/oyvey/features/modules/movement/AutoWalk.java
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/AutoWalk.java
@@ -1,9 +1,14 @@
 package me.alpha432.oyvey.features.modules.movement;
 
+import me.alpha432.oyvey.OyVey;
+import me.alpha432.oyvey.features.commands.Command;
 import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.settings.Setting;
+import me.alpha432.oyvey.util.models.Timer;
+import net.minecraft.util.Formatting;
 
 public class AutoWalk extends Module {
+
     private final Setting<Boolean> autoJump = this.register(new Setting<>("AutoJump", false));
 
     public AutoWalk() {
@@ -11,21 +16,21 @@ public class AutoWalk extends Module {
     }
 
     @Override
-    public void onUpdate() {
-        mc.options.forwardKey.setPressed(true);
-        mc.options.sprintKey.setPressed(true);
-        if (autoJump.getValue())
-        mc.options.jumpKey.setPressed(true);
-
-
+    public void onTick() {
+            mc.options.forwardKey.setPressed(true);
+            if (autoJump.getValue()) {
+                mc.options.jumpKey.setPressed(true);
+            }
+            if (mc.player.horizontalCollision) {
+                String message = Formatting.DARK_RED + "[!] "  + Formatting.WHITE + "Вы врезались в блок!";
+                Command.sendMessage(message);
+            }
     }
 
     @Override
     public void onDisable() {
-        mc.options.forwardKey.setPressed(false);
-        mc.options.jumpKey.setPressed(false);
-        mc.options.sprintKey.setPressed(false);
-
+            mc.options.forwardKey.setPressed(false);
+            mc.options.jumpKey.setPressed(false);
     }
 }
 
